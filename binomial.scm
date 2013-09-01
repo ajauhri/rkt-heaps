218805675510d6dd8d3cc648f79d83c8b12e833b
ajauhri <abhinavjauhri@gmail.com>
2013-09-01 02:24:29 -0700

    combine function and other helper functionas added. test cases due for meld

--- binomial.scm
+++ binomial.scm
@@ -12,7 +12,7 @@
 
 ;;; Description: Return the min element in the heap
 ;; Notes: 
-;; - vector-ref is takes constant time - www.eecs.bekeley.edu/~bh/ssch23/vectors.html 
+;; - vector-ref is takes constant time - www.eecs.berkeley.edu/~bh/ssch23/vectors.html 
 ;; - if actual argument is not a heap, function returns 0
 (define (findmin h)
   (if (checkheap? h)
@@ -25,8 +25,54 @@
 
 (define (checkheap? h)
   (and (vector? (car h)) (< (cdr h) (vector-length (car h)))))
+
 ;; meld function:
-;; - The heap is structured in a pair with `car` pointing to a vector & `cdr` pointing to the index of the smallest root of a binomial tree in the heap. In cases where the heap consists of just one tree the root has to have the smallest element in which the having this pair structure may seem irrelevant but let's just work on it for now. Meld operation is a bit complicated so I want to list out the minor details in my mind before implementation.
+;; - The heap is structured in a pair with `car` pointing to a vector & `cdr` pointing to the index of the smallest root of a binomial tree in the heap. 
+;; Meld operation is a bit complicated.
 ;; - if both heaps have b_i's, then the resultant heap will get one b_(i+1). 
 ;; - if more than 2, then as before two will combine to form b_(i+1) and any one will stay as b_i
-;(define (meld h1 h2))
+(define (meld h1 h2)
+  (if (areheaps? (list h1 h2))
+    (if (< (findmin h1) (findmin h2))
+      (cons (combine (car h1) (car h2)) 0))
+    '()))
+
+;; takes vectors only
+(define (combine v1 v2 i)
+  (cond ((and (not empty-slot? v1 i) (not empty-slot? v2 i))
+         (let ((result (make-vector (expt 2 i) -1))) 
+          (cond ((<= (value-at v1 i) (value-at v2 i))
+                 (vector-append result 
+                                (subvector v1 (root-index i) (root-index (+ i 1))) 
+                                (subvector v2 (root-index i) (root-index (+ i 1)))
+                                (combine v1 v2 (+ i 1))))
+                ((> (value-at v1 i) (value-at v2 i))
+                 (vector-append result 
+                                (subvector v2 (root-index i) (root-index (+ i 1)))
+                                (subvector v1 (root-index i) (root-index (+ i 1)))
+                                (combine v1 v2 (+ i 1)))))))
+        ((and (empty-slot? v1 i) (empty-slot? v2 i))
+         (vector-append (make-vector (expt 2 i) -1)
+                        (combine v1 v2 (+ i 1))))
+        ((or (empty-slot? v1 i) (empty-slot? v2 i))
+         (cond ((empty-slot? v1 i) 
+                (vector-append (subvector v2 (root-index i) (root-index (+ i 1))) 
+                               (combine v1 v2 (+ i 1))))
+               ((empty-slot? v2 i)
+                (vector-append (subvector v1 (root-index i) (root-index (+ i 1)))
+                               (combine v1 v2 (+ i 1))))))))
+
+
+(define (root-index i)
+  (- (expt 2 i) 1))
+
+(define (empty-slot? vec i)
+  (eq? (value-at vec i) -1))
+
+(define (value-at vec i)
+  (vector-ref vec (- (expt 2 i) 1)))
+
+(define (vector-empty? vec)
+  (if (> (vector-length vec) 0)
+    #t
+    #f))
--- tests/binomial_test.scm
+++ tests/binomial_test.scm
@@ -11,5 +11,24 @@
   (define-test (findmin)
     "Checking findmin"
     (assert-= 0 (findmin (cons 3 4)))
-    (assert-= 1 (findmin (makeheap 1)))))
+    (assert-= 1 (findmin (makeheap 1))))
+  
+  (define-test (meld)
+    "Checking meld for not valid heaps"
+    (assert-true (null? (meld (makeheap 1) (cons 1 2)))))
+  
+  (define-test (root-index)
+               "Checking root index gives the correct index for root value in the vector"
+               (assert-equal 0 (root-index 0)))
+  
+  (define-test (empty-slot)
+               "Checking empty slot in the vector"
+               (assert-true (empty-slot? (make-vector 1 -1) 0))
+               (assert-false (empty-slot? (vector '1 '2 '3) 1))
+               (assert-true (empty-slot? (vector '1 '2 '3 '-1) 2)))
+  
+  (define-test (value-at)
+               "Checking for correct value extraction from the vector"
+               (assert-equal 2 (value-at (vector '1 '2 '3) 1))))
+
 (run-registered-tests)
