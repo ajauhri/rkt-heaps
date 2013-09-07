44f3a526536c28a75f9b3e08bd64242105b81b3e
ajauhri <abhinavjauhri@gmail.com>
2013-09-07 00:45:37 -0700

    tests fixed and refined fn prototypes

--- binomial.scm
+++ binomial.scm
@@ -16,15 +16,23 @@
 ;;- vector-ref is takes constant time - www.eecs.berkeley.edu/~bh/ssch23/vectors.html 
 ;;- if actual argument is not a heap, function returns 0
 (define (findmin h)
-  (if (checkheap? h)
+  (if (heap? h)
     (vector-ref (car (car h)) (cdr h))
     #f))
 
 
+;;Insert a positive integer to an existing heap and returns the resultant heap
+(define (insert h i)
+  (meld h (makeheap i)))
+
+(define (deletemin h)
+  (if (heap? h)
+    #t
+    #f))
+
 ;;Returns a heap which a combination of the two heaps provided as arguments to the method. 
-;; todo: find the resultant min. element of the combined heap
 (define (meld h1 h2)
-  (if (areheaps? (list h1 h2))
+  (if (heaps? h1 h2)
     (let ()
       (define (couple v1 v2 s1 s2 carry res i)
               (let ((b1 (root-slot-valid? s1)) 
@@ -42,35 +50,42 @@
                                       ((and b1 b2 (not b3)) (cons (constructcarry v1 v2 i) (vector-append res (make-vector i #f))))
                                       ((and b1 b2 b3) (cons (propcarry v2 carry i) (vector-append res (propres v1 i)))))))
                           (couple v1 v2 (floor (/ s1 2)) (floor (/ s2 2)) (car newargs) (cdr newargs) (* i 2)))))))
-      (let ((res (cons (couple (car (car h1)) (car (car h2)) (cdr (car h1)) (cdr (car h2)) #() #() 1) (+ (cdr (car h1)) (cdr (car h2))))))
-       (define (getmin h len i m)
-         (if (= len 0) 
-           (- m 1) 
-           (getmin h (floor (/ len 2)) (* i 2) (cond ((and (= (modulo len 2) 1) (eq? m #f)) i)
-                                                     ((= (modulo len 2) 0) m)
-                                                     (else
-                                                       (if (< (vector-ref h (- i 1)) (vector-ref h (- m 1))) i m))))))
-       (cons res (getmin (car res) (cdr res) 1 #f))))
+      (let ((res (cons (couple (car (car h1)) (car (car h2)) (cdr (car h1)) (cdr (car h2)) #() #() 1) 
+                       (+ (cdr (car h1)) (cdr (car h2))))))
+       (cons res 
+             (getmin (car res) (cdr res) 1 #f))))
     #f))
-
-
-;;Insert a positive integer to an existing heap and returns the resultant heap
-(define (insert h i)
-  (meld h (makeheap i)))
 
 ;;;
 ;;; Helper functions
 ;;;
 
-;; Predicate based on the structural formation of binomial heap.
-(define (areheaps? h)
-  (if (list? h)
-    (for-all? h checkheap?)
-    #f))
+(define (getmin v len i m) 
+  (if (= len 0)
+    (- m 1)
+    (getmin v (floor (/ len 2)) (* i 2) (cond ((and (= (modulo len 2) 1) (eq? m #f)) i)
+                                              ((= (modulo len 2) 0) m)
+                                              (else
+                                                (if (< (vector-ref v (- i 1)) (vector-ref v (- m 1))) i m))))))
 
-;; Asserts based on the underlying implementation here, whether the given parameter is structurally correct as a heap or not. Does not check whether the heap condition is violated or not.
-(define (checkheap? h)
-  (and (vector? (car (car h))) (< (cdr h) (vector-length (car (car h))))))
+;; Predicate based on the structural formation of binomial heap.
+(define (heaps? . h)
+  (for-all? h heap?))
+
+;; Asserts whether the given argument is a valid heap or not
+;; Commentary:
+;; - based on the underlying implementation structural formation of a heap, it checks whether they are correctly specified in the argument
+;; - it is checked whether the values of the heap should be stored in a vector
+;; - it is checked that the index of the min element is at most equal to vector-lenght/2 (since the vector length will have padded #f in all trees whose root index is represented by 0 bit when writing the number of elements in binary representation 
+;; - it is checked that the number of valid elements in the heap are at most the length of the entire forest(vector) 
+;; - it is checked that the min is correctly indexed 
+;; - doesn't check whether the heap condition is maintained or not
+;; - although this is not a thorough check of the validity of the heap condition, its use is relevant since it validates what is passed to meld and deletemin methods 
+(define (heap? h)
+  (and (vector? (car (car h))) 
+       (<= (cdr h) (floor (/ (vector-length (car (car h))) 2))) 
+       (<= (cdr (car h)) (vector-length (car (car h))))
+       (= (getmin (car (car h)) (cdr (car h)) 1 #f) (cdr h))))
 
 ;; Returns ith bit of v
 (define (propres v i)
--- tests/binomial_test.scm
+++ tests/binomial_test.scm
@@ -3,33 +3,30 @@
 
 (in-test-group
   binomial-heap
-  (define-test (isheap)
-    "Checking isheap? works fine"
-    (assert-true (areheaps? (list (makeheap 1))))
-    (assert-false (areheaps? (list (cons 1 0)))))
   
+  (define-test (makeheap)
+    "Checking makeheap"
+    (assert-true (equal? (cons (cons (vector 1) 1) 0) (makeheap 1))))
+   
   (define-test (findmin)
     "Checking findmin"
-    (assert-= 0 (findmin (cons 3 4)))
-    (assert-= 1 (findmin (makeheap 1))))
+    (assert-= 1 (findmin (makeheap 1))) 
+    (let ((h (insert (insert (makeheap 3) 4) 2))) (assert-= 2 (findmin h))))
   
+  (define-test (insert)
+    "Checking insert"
+    (assert-true (heap? (insert (makeheap 1) 2))))
+  
+  (define-test (heaps?)
+    "Checking heaps? works fine"
+    (assert-true (heaps? (makeheap 1))) 
+    (assert-false (heap? (cons (cons (vector #f 1 1) 2) 0)))
+    (assert-true (heaps? (makeheap 1) (makeheap 2) (makeheap 3))))
+ 
   (define-test (meld)
     "Checking meld for not valid heaps"
-    (assert-true (null? (meld (makeheap 1) (cons 1 2))))
-    (assert-equal (cons (vector '() 1 1) 0) (meld (makeheap 1) (makeheap 1))))
-  
-  (define-test (root-index)
-               "Checking root index gives the correct index for root value in the vector"
-               (assert-equal 0 (root-index 0)))
-  
-  (define-test (empty-slot)
-               "Checking empty slot in the vector"
-               (assert-true (empty-slot? (make-vector 1 '()) 0))
-               (assert-false (empty-slot? (vector '1 '2 '3) 1))
-               (assert-true (empty-slot? (vector '1 '2 '3 '()) 2)))
-  
-  (define-test (value-at)
-               "Checking for correct value extraction from the vector"
-               (assert-equal 2 (value-at (vector '1 '2 '3) 1))))
+    (assert-false (meld (makeheap 1) (cons (cons #(1 2 3) 3) 2)))
+    (assert-false (meld (makeheap 1) (cons (cons '(1 2 3) 3) 0)))
+    (assert-true (equal? (cons (cons (vector #f 1 1) 2) 1) (meld (makeheap 1) (makeheap 1))))))
 
 (run-registered-tests)
