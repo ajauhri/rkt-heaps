a554cfe8e13b79d0fb8ede1a560dc7d046864050
ajauhri <abhinavjauhri@gmail.com>
2013-09-08 19:40:00 -0700

    cleaning file structuring

---
+++ tests/binomial_test.rkt
@@ -0,0 +1,34 @@
+#lang racket
+(require rackunit "../src/binomial.rkt")
+
+(test-suite
+  "binomail heap"
+  (test-case
+    "Checking makeheap"
+    (check-true (equal? (cons (cons (vector 1) 1) 0) (makeheap 1))))
+   
+  (test-case
+    "Checking findmin"
+    (check-eq? 1 (findmin (makeheap 1))) 
+    (let ((h (insert (insert (makeheap 3) 4) 2))) (check-eq? 2 (findmin h))))
+  
+  (test-case
+    "Checking insert"
+    (check-true (heaps? (insert (makeheap 1) 2))))
+  
+  (test-case
+    "Checking deletemin"
+    (check-false (deletemin (cons (cons (vector 2 3 4) 3) 2))))
+  
+  (test-case
+    "Checking heaps? works fine"
+    (check-true (heaps? (makeheap 1))) 
+    (check-false (heaps? (cons (cons (vector #f 1 1) 2) 0)))
+    (check-true (heaps? (makeheap 1) (makeheap 2) (makeheap 3))))
+ 
+  (test-case
+    "Checking meld for not valid heaps"
+    (check-false (meld (makeheap 1) (cons (cons #(1 2 3) 3) 2)))
+    (check-false (meld (makeheap 1) (cons (cons '(1 2 3) 3) 0)))
+    (check-true (equal? (cons (cons (vector #f 1 1) 2) 1) (meld (makeheap 1) (makeheap 1))))))
+
