abde3449b481a2e512e569619aebbb163af675ea
ajauhri <abhinavjauhri@gmail.com>
2013-09-14 03:56:34 -0700

    plots getting somewhere...

--- benchmarks/binomial_benchmarks.rkt
+++ benchmarks/binomial_benchmarks.rkt
@@ -1,7 +1,7 @@
 #! /usr/bin/racket
 #lang racket
-
 (require plot "../src/binomial.rkt")
+(provide run-findmin plot-findmin)
 
 (define R 4294967087)
 
@@ -9,25 +9,37 @@
   (for/vector #:length s ([i (in-range s)]) (random R)))
 
 (define (make-large-heap v)
-  (for/fold ([h (makeheap (random R))]) ((val (in-vector v))) (insert h val)))
+  (for/fold ([h (makeheap (random R))]) ([val (in-vector v)]) (insert h val)))
 
-(define (b-makeheap v)
+(define (time-makeheap v)
   (let ((start (current-inexact-milliseconds)) (r (makeheap v)) (end (current-inexact-milliseconds)))
    (- end start)))
  
-(define (b-findmin h)
+(define (time-findmin h)
   (let ((start (current-inexact-milliseconds)) (r (findmin h)) (end (current-inexact-milliseconds)))
    (- end start))) 
 
-(define (b-insert v)
-  (time
-    (for/fold ([h (makeheap (random R))]) ([val (in-vector v)])
-            (insert h val))))
+(define (time-insert h i)
+  (let ((start (current-inexact-milliseconds)) (r (insert h i)) (end (current-inexact-milliseconds)))
+   (- end start)))
 
-(define (b-deletemin h)
+(define (time-deletemin h)
   (let ((start (current-inexact-milliseconds)) (r (deletemin h)) (end (current-inexact-milliseconds)))
    (- end start)))
 
-(define (b-meld h1 h2)
+(define (time-meld h1 h2)
   (let ((start (current-inexact-milliseconds)) (r (meld h1 h2)) (end (current-inexact-milliseconds)))
    (- end start)))
+
+(define (run-findmin ssize esize step freq)
+  (for/vector #:length (/ (- esize ssize) step) ([i (in-range ssize esize step)])
+              (let ((h (make-large-heap (make-rand-vector (- i 1)))))
+               (/ (for/sum ([j (in-range freq)]) (time-findmin h)) freq))))
+
+(define (plot-findmin ssize esize step freq)
+  (plot (lines
+          (reverse
+           (let ((v (run-findmin ssize esize step freq)))
+           (for/fold ([lst (list (vector 0 0))]) ([i (in-range ssize esize step)]
+                                                 [j (in-range (vector-length v))])
+                     (cons (vector i (vector-ref v j)) lst)))))))
