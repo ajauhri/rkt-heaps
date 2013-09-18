6e2c7acb9c9b5dfe95c48401f4bf0dd663e5797e
ajauhri <abhinavjauhri@gmail.com>
2013-09-18 14:00:08 -0700

    meld for fibonacci added

--- src/fibonacci_helper.rkt
+++ src/fibonacci_helper.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (require racket/mpair)
-(provide make-node get-roots get-node-vector get-value heap? heaps?)
+(provide make-node get-roots get-node-vector get-value heap? heaps? get-min-index)
 
 (define (make-node val)
   (vector #f val #f (vector)))
@@ -14,6 +14,9 @@
 
 (define (get-value n)
   (vector-ref n 1))
+
+(define (get-min-index h)
+  (mcdr (mcar h)))
 
 (define (heap? h) #t)
 
