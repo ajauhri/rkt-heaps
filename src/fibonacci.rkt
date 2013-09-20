87fa0cd24059401a58c3e8a3d12f2eacda71bb65
ajauhri <abhinavjauhri@gmail.com>
2013-09-19 17:01:08 -0700

    simplicity is heaven

--- src/fibonacci.rkt
+++ src/fibonacci.rkt
@@ -7,45 +7,37 @@
 ;; meld
 ;; decrement
 ;; delete
-(require racket/mpair "fibonacci_helper.rkt" compatibility/mlist)
+(require "fibonacci_helper.rkt")
 
-(provide makeheap findmin insert! meld)
+(provide makeheap findmin insert meld)
 
 (define (makeheap val)
   (let ((rts (vector (vector 0))))
-   (mcons 
-     (mcons (vector (make-node val)) 0) 
-     rts)))
+   (cons (cons (vector (make-node val)) 0) 
+         rts)))
 
 (define (findmin h)
   (cond ((not (heap? h)) (raise-argument-error 'findmin "heap?" h))
-        (else (get-value (vector-ref (get-node-vector h) (mcdr (mcar h)))))))
+        (else (get-value (vector-ref (get-node-vector h) (cdr (car h)))))))
 
-(define (insert! h val)
+(define (insert h val)
   (cond ((not (heap? h)) (raise-argument-error 'insert! "heap?" 0 h val))
         ((not (number? val)) (raise-argument-error 'insert! "number?" 1 h val))
         (else
-          (let ((rts (get-root-vector h))
-                (zerorank (vector-ref (get-root-vector h) 0))
-                (node-vector (get-node-vector h)))
-            (vector-set! rts 0 (vector-append zerorank (vector (vector-length node-vector))))
-            (when (< val (findmin h)) (set-mcdr! (mcar h) (vector-length node-vector)))
-            (set-mcar! (mcar h) (vector-append node-vector (vector (make-node val))))))))
+          (meld h (makeheap val)))))
 
 ;(define (deletemin h)
-  ;first make the heap, get min's children and do a meld with remaining elements of heap and children
+;first make the heap, get min's children and do a meld with remaining elements of heap and children
 ;  )
 
 (define (meld h1 h2)
   (cond ((not (heap? h1)) (raise-argument-error 'meld "heap?" 0 h1 h2))
         ((not (heap? h2)) (raise-argument-error 'meld "heap?" 1 h1 h2))
         (else
-          (let ((rootlenh1 (vector-length (get-root-vector h1)))
-                (rootlenh2 (vector-length (get-root-vector h2))))
-            (let ((rts (if (<= rootlenh1 rootlenh2) (make-root-vector h2 h1) (make-root-vector h1 h2)))
-                  (nodevec (vector-append (get-node-vector h2) (get-node-vector h1)))
-                  (minind (if (<= (findmin h1) (findmin h2)) 
-                            (get-new-min h1 h2)
-                            (get-new-min h2 h1))))
-              (mcons (mcons nodevec minind) rts))))))        
+          (let ((rts (make-root-vector h1 h2))
+                (nodevec (vector-append (get-node-vector h1) (get-node-vector h2)))
+                (minind (if (> (findmin h1) (findmin h2)) 
+                          (+ (vector-length (get-node-vector h1)) (get-min-index h2))
+                          (get-min-index h1))))
+            (cons (cons nodevec minind) rts)))))
 
--- src/fibonacci_helper.rkt
+++ src/fibonacci_helper.rkt
@@ -1,42 +1,34 @@
 #lang racket
 
-(require racket/mpair)
-(provide make-node get-root-vector get-node-vector get-value get-min-index heap? make-root-vector get-new-min)
+(provide make-node get-root-vector get-node-vector get-value get-min-index heap? make-root-vector)
 
 (define (make-node val)
   (vector #f val #f (vector)))
 
 (define (get-root-vector h)
-  (mcdr h))
+  (cdr h))
 
 (define (get-node-vector h)
-  (mcar (mcar h)))
+  (car (car h)))
 
 (define (get-value n)
   (vector-ref n 1))
 
 (define (get-min-index h)
-  (mcdr (mcar h)))
+  (cdr (car h)))
 
 (define (heap? h) #t)
 
-; pre-condition: length of the root vector for h2 should be small or equal to the length of root vector for h1
 (define (make-root-vector h1 h2 )
   (let ((rootvech1 (get-root-vector h1))
         (rootvech2 (get-root-vector h2))
         (sizeh1 (vector-length (get-node-vector h1))))
-    (if (< (vector-length rootvech1) (vector-length rootvech2))
-      (raise-argument-error 'make-root-vector "(< (vector-length rootvech1) (vector-length rootvech2)" h1)
-      (for/vector #:length (vector-length rootvech2) ([i (in-vector rootvech1)]
-                                                      [j (in-vector rootvech2)])
-                  (vector-append i (vector-map (lambda (val) (+ val sizeh1)) j))))))
-
-; pre-condition: (<= (findmin h1) (findmin h2))
-(define (get-new-min h1 h2)
-  (if (> (get-value (vector-ref (get-node-vector h1) (mcdr (mcar h1)))) 
-         (get-value (vector-ref (get-node-vector h2) (mcdr (mcar h2)))))
-    (raise-argument-error 'get-min-index "(> (findmin h1) (findmin h2))" h1)
-    (if (< (vector-length (get-root-vector h1)) (vector-length (get-root-vector h2)))
-      (+ (vector-length (get-node-vector h2)) (get-min-index h1))
-      (get-min-index h1))))
-
+    (vector-append (for/vector ([i (in-vector rootvech1)]
+                                [j (in-vector rootvech2)])
+                               (vector-append i (vector-map (lambda (val) (+ val sizeh1)) j)))
+                   (cond ((< (vector-length rootvech1) (vector-length rootvech2))
+                          (for/vector ([i (in-vector (vector-take-right rootvech2 (vector-length rootvech1)))])
+                                      (vector-map (lambda (val) (+ val sizeh1)) i)))
+                         ((> (vector-length rootvech1) (vector-length rootvech2))
+                          (vector-take-right rootvech1 (vector-length rootvech2)))
+                         (else #())))))
