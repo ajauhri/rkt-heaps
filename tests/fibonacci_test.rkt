62f004554ab7134d9882e0e86b1d04a3111f001f
ajauhri <abhinavjauhri@gmail.com>
2013-10-06 13:46:48 -0700

    tests working for fibonacci

--- tests/fibonacci_test.rkt
+++ tests/fibonacci_test.rkt
@@ -6,10 +6,10 @@
     "fibonacci heap"
     (test-case
       "Checking makeheap creates a opaque structure"
-      (define n (node 1 #f #() #f))
+      (define n (node 1 #f #() #f #f #f))
       (set-node-left! n n)
       (set-node-right! n n)
-      (check-false (equal? (fi-heap n (vector (vector n)) 1) (fi-makeheap 1))))
+      (check-false (equal? (fi-heap n 1) (fi-makeheap 1))))
 
     (test-case
       "Checking findmin with one value"
@@ -22,7 +22,7 @@
        (set! h (fi-insert h 4))
        (set! h (fi-insert h 5))
        (fi-deletemin! h)
-       (define n (vector-ref (fi-node-children (fi-heap-minref h) 0)))
+       (define n (vector-ref (node-children (fi-heap-minref h)) 1)) 
        (fi-decrement! h n 2) 
        (fi-delete! h n)
        (check-eq? 2 (fi-findmin h))))
@@ -47,7 +47,7 @@
        (set! h (fi-insert h 4))
        (set! h (fi-insert h 5))
        (fi-deletemin! h)
-       (define n (vector-ref (vector-ref (fi-heap-roots h) 2) 0))
+       (define n (fi-heap-minref h))
        (define c1 (vector-ref (fi-node-children n) 1))
        (define c2 (vector-ref (fi-node-children c1) 0))
        (fi-decrement! h c2 4)
@@ -61,16 +61,6 @@
        (check-true (eq? (fi-heap-size h) 1))
        (check-true (eq? (fi-findmin h) 2))))
 
-    (test-case 
-      "meld heap with different root vector sizes"
-      (let ((h (fi-makeheap 1)))
-       (set! h (fi-insert h 2))
-       (set! h (fi-insert h 3))
-       (set! h (fi-insert h 4))
-       (fi-deletemin! h)
-       (set! h (fi-insert h 5))
-       (check-true (= (vector-length (fi-heap-roots h)) (+ 1 (ceiling (/ (log (fi-heap-size h)) (log 2))))))))
-
     (test-case
       "If two children of a root node are deleted, it shouldn't be marked"
       (let ((h (fi-meld (fi-makeheap 1) (fi-makeheap 2))))
@@ -78,24 +68,11 @@
        (set! h (fi-insert h 4))
        (set! h (fi-insert h 5))
        (fi-deletemin! h)
-       (define n (vector-ref (vector-ref (fi-heap-roots h) 2) 0))
+       (define n (fi-heap-minref h))
        (define c1 (vector-ref (fi-node-children n) 0))
        (define c2 (vector-ref (fi-node-children n) 1))
        (fi-delete! h c1)
        (fi-delete! h c2)
        (check-false (node-marked n))
        (fi-deletemin! h)
-       (check-eq? 5 (fi-findmin h))))))
-
-
-
-
-(define h (fi-meld (fi-makeheap 1) (fi-makeheap 2)))
-(define h (fi-insert h 3))
-(fi-deletemin! h)
-(fi-deletemin! h)
-(fi-findmin h)
-(define h (fi-insert h 4))
-(define h (fi-insert h 5))
- 
-
+       (check-eq? 4 (fi-findmin h))))))
